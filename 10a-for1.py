26cb3c6b5b7e2e3641fcdc4107694648244ddd35
VNYZ <forvnyz@gmail.com>
2020-11-11 00:12:31 +0300

    Add files via upload

---
+++ 10a-for1.py
@@ -0,0 +1,11 @@
+metin="Tecrübe tek bilgi kaynağıdır. -Albert Einstein"
+
+sayac=0
+
+for x in metin:
+  if x==" ":
+    sayac+=1 #sayac=sayac+1
+
+print("metnin içinde",sayac+1,"tane kelime var")
+
+
